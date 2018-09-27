0fe554d33f36ea0a92a532bd50dcf525f52641b0
bcchildr@us.ibm.com <bcchildr@us.ibm.com>
2018-09-26 22:49:21 -0500

    Create and object at the bottom of the class

--- main.ts
+++ main.ts
@@ -3,10 +3,15 @@ class Point {
   y: number;
 
   draw() {
-    // ...
+    console.log("X: " + this.x + ", Y: " + this.y);
   }
 
   getDistance(another: Point) {
     // ...
   }
 }
+
+let point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
