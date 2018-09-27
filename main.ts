2471fc54a22df0814958e47d64aa5f7096ce24db
bcchildr@us.ibm.com <bcchildr@us.ibm.com>
2018-09-26 21:53:30 -0500

    Create a lambda function aka arrow function

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-let message;
-message = "abc";
-let endsWithC = (<string>message).endsWith("c");
-let alternativeWay = (message as string).endsWith("c");
+let log = function(message) {
+    console.log(message);
+}
+
+let doLog = (message) => console.log(message);
